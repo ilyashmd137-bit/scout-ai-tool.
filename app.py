4974d7216bbca87b922a18ed433a416cb4a20601
ilyashmd137-bit <ilyashmd137@gmail.com>
2026-06-14 12:07:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title( اجعله "المحلل الذكي للإعلانات - V1". ")
+st.title("المحلل الذكي للإعلانة)
 st.write("أهلاً بك في أول مشروع لي!")
 
 text = st.text_input("اكتب شيئاً هنا وسأقوم بتحليله:")
